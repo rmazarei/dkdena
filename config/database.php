b36df0340bb5068bf5a21438a2f35690dacccab9
Rouhollah Mazarei <rouhollah@gmail.com>
2023-11-25 09:42:02 +0330

    Add second db connection
    Update config/database.php so the project can use it for comments

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'dkdena_comment' => [
+            'driver' => 'mysql',
+            'url' => env('COMMENT_DATABASE_URL'),
+            'host' => env('COMMENT_DB_HOST', '127.0.0.1'),
+            'port' => env('COMMENT_DB_PORT', '3306'),
+            'database' => env('COMMENT_DB_DATABASE', 'forge'),
+            'username' => env('COMMENT_DB_USERNAME', 'forge'),
+            'password' => env('COMMENT_DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
